8e86f124749836c755d3582e5d5b68a452636d6c
Devansh Gupta <devanshhub@gmail.com>
2025-08-29 20:30:13 +0530

    hid link

--- frontend/src/data/bannerData.js
+++ frontend/src/data/bannerData.js
@@ -2,7 +2,7 @@ export const bannerSlides = [
     {
         title: 'iPhone 16 Pro',
         subtitle: 'Up to 10% off Voucher',
-        shopLink: '/products/iphone-16-pro',
+        //shopLink: '/products/iphone-16-pro',
         imageUrl: 'https://cdsassets.apple.com/live/7WUAS350/images/tech-specs/121032-iphone-16-pro-max.png',
         bgColor: 'from-yellow-300 to-gold-600',
         textColor: 'text-black'
@@ -10,7 +10,7 @@ export const bannerSlides = [
     {
         title: 'Apple Watch Ultra 2',
         subtitle: 'Next-level adventure.',
-        shopLink: '/products/apple-watch-ultra-2',
+        //shopLink: '/products/apple-watch-ultra-2',
         imageUrl: 'https://www.aptronixindia.com/pub/media/catalog/product/t/i/titanium_orange_ocean_band_pdp_image_position-1__en-us-removebg-preview_1.png',
         bgColor: 'from-orange-300 to-gray-500',
         textColor: 'text-black'
@@ -18,7 +18,7 @@ export const bannerSlides = [
     {
         title: 'MacBook Air M3',
         subtitle: 'Lean. Mean. M3 machine.',
-        shopLink: '/products/macbook-air-m3',
+        //shopLink: '/products/macbook-air-m3',
         imageUrl: 'https://www.indiaistore.com/files/uploads/products/macbook-air-m3/images/hero_small_2x.png?1708555710106',
         bgColor: 'from-green-300 to-blue-300',
         textColor: 'text-black'
@@ -26,7 +26,7 @@ export const bannerSlides = [
     {
         title: 'iPad Pro',
         subtitle: 'Thinpossible. Bigpossible.',
-        shopLink: '/products/ipad-pro',
+        //shopLink: '/products/ipad-pro',
         imageUrl: 'https://www.sunrise.ch/var/commerce/products/web/Default/en/apple_ipad_pro_11256gbcellular2020parent/picture.1612199642232.transform/original/20615_product_image1000x1000png.png',
         bgColor: 'from-yellow-300 to-pink-300',
         textColor: 'text-black'
